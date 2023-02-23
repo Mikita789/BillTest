72ec08a1a4df9560b36f13c077d33023eb4005a7
Mikita <nikita_popow_1993@mail.ru>
2023-02-23 16:55:32 +0300

    основная часть

--- Bill/ViewController.swift
+++ Bill/ViewController.swift
@@ -8,12 +8,210 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var topView:UIView!
+    var billLabel:UILabel!
+    var billTextField:UITextField!
+    
+    var bottView:UIView!
+    var persentLabel:UILabel!
+    var countPersonLabel:UILabel!
+    var currentCountPerson:UILabel!
+    var stepperPerson:UIStepper!
+    var selectCountPercent:UISegmentedControl!
+    
+    var resultButton:UIButton!
+    
+    var percents = ["0%", "5%", "10%", "20%"]
+    var startValuePerson = 2.0
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print(view.center.x)
+        
+        
+        topViewSettings()
+        bottViewSettings()
+        billLabelSettings()
+        billTextFieldSettings()
+        persentLabelSettings()
+        selectCountPercentSettings()
+        countPersonLabelSettings()
+        currentCountPersonSettings()
+        stepperPersonSettings()
+        resultButtonSettings()
+        
     }
+    //скрывает клавиатуру при нажатии на другое место
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.billTextField.resignFirstResponder()
+    }
+    private func topViewSettings(){
+        topView = UIView()
+        self.view.addSubview(topView)
+        topView.translatesAutoresizingMaskIntoConstraints = false
+        
+        topView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        topView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        topView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        topView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.3).isActive = true
+        
+        topView.backgroundColor = .white
+    }
+    
+    private func bottViewSettings(){
+        bottView = UIView()
+        self.view.addSubview(bottView)
+        bottView.translatesAutoresizingMaskIntoConstraints = false
+        bottView.topAnchor.constraint(equalTo: topView.bottomAnchor).isActive = true
+        bottView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        bottView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        bottView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        
+        bottView.backgroundColor = UIColor(red: 0.57, green: 0.81, blue: 0.77, alpha: 0.5)
+    }
+    
+    private func billLabelSettings(){
+        billLabel = UILabel()
+        topView.addSubview(billLabel)
+        billLabel.translatesAutoresizingMaskIntoConstraints = false
+        billLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10).isActive = true
+        billLabel.leftAnchor.constraint(equalTo: topView.leftAnchor, constant: 20).isActive = true
+        billLabel.heightAnchor.constraint(equalTo: topView.heightAnchor, multiplier: 0.2).isActive = true
+        billLabel.widthAnchor.constraint(equalTo: topView.widthAnchor, multiplier: 0.5).isActive = true
+        
+        billLabel.text = "Введите сумму счета"
+        billLabel.textColor = .lightGray
+    }
+    
+    private func billTextFieldSettings(){
+        billTextField = UITextField()
+        topView.addSubview(billTextField)
+        billTextField.translatesAutoresizingMaskIntoConstraints = false
+        billTextField.topAnchor.constraint(equalTo: billLabel.bottomAnchor, constant: 10).isActive = true
+        billTextField.leftAnchor.constraint(equalTo: topView.leftAnchor, constant: 10).isActive = true
+        billTextField.centerXAnchor.constraint(equalTo: topView.centerXAnchor).isActive = true
+        billLabel.bottomAnchor.constraint(equalTo: topView.bottomAnchor, constant: 10).isActive = true
+        
+        billTextField.placeholder = "Enter value bill"
+        billTextField.textAlignment = .center
+        billTextField.textColor = .systemGreen
+        billTextField.font = .systemFont(ofSize: 50)
+        billTextField.keyboardType = .numberPad
+    }
+    
+    private func persentLabelSettings(){
+        persentLabel = UILabel()
+        self.view.addSubview(persentLabel)
+        persentLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            persentLabel.topAnchor.constraint(equalTo: bottView.topAnchor, constant: 10),
+            persentLabel.leftAnchor.constraint(equalTo: bottView.leftAnchor, constant: 20),
+            persentLabel.widthAnchor.constraint(equalTo: bottView.widthAnchor, multiplier: 0.5),
+            persentLabel.heightAnchor.constraint(equalTo: billLabel.heightAnchor)
+        ])
+        
+        persentLabel.text = "Выберите чаевые"
+        persentLabel.textColor = .lightGray
+    }
+    
+    private func selectCountPercentSettings(){
+        selectCountPercent = UISegmentedControl(items: percents)
+        bottView.addSubview(selectCountPercent)
+        selectCountPercent.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            selectCountPercent.topAnchor.constraint(equalTo: persentLabel.bottomAnchor, constant: 10),
+            selectCountPercent.leftAnchor.constraint(equalTo: bottView.leftAnchor),
+            selectCountPercent.centerXAnchor.constraint(equalTo: bottView.centerXAnchor),
+            selectCountPercent.heightAnchor.constraint(equalToConstant: 30)
 
-
+        ])
+        selectCountPercent.selectedSegmentIndex = 0
+    }
+    
+    private func countPersonLabelSettings(){
+        countPersonLabel = UILabel()
+        bottView.addSubview(countPersonLabel)
+        countPersonLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            countPersonLabel.topAnchor.constraint(equalTo: selectCountPercent.bottomAnchor, constant: 10),
+            countPersonLabel.leftAnchor.constraint(equalTo: bottView.leftAnchor, constant: 20),
+            countPersonLabel.heightAnchor.constraint(equalTo: persentLabel.heightAnchor),
+            countPersonLabel.widthAnchor.constraint(equalTo: persentLabel.widthAnchor),
+        ])
+        
+        countPersonLabel.textColor = .lightGray
+        countPersonLabel.text = "Колличество друзей"
+    }
+    
+    private func currentCountPersonSettings(){
+        currentCountPerson = UILabel()
+        bottView.addSubview(currentCountPerson)
+        currentCountPerson.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            currentCountPerson.topAnchor.constraint(equalTo: countPersonLabel.bottomAnchor, constant: 10),
+            currentCountPerson.heightAnchor.constraint(equalTo: bottView.heightAnchor, multiplier: 0.08),
+            currentCountPerson.widthAnchor.constraint(equalTo: bottView.widthAnchor, multiplier: 0.09),
+            currentCountPerson.rightAnchor.constraint(equalTo: bottView.centerXAnchor, constant: -20)
+        ])
+        currentCountPerson.text = "\(Int(self.startValuePerson))"
+        currentCountPerson.textColor = .systemGreen
+        currentCountPerson.font = .systemFont(ofSize: 30)
+        currentCountPerson.textAlignment = .center
+        
+    }
+    
+    private func stepperPersonSettings(){
+        stepperPerson = UIStepper()
+        bottView.addSubview(stepperPerson)
+        stepperPerson.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            stepperPerson.centerYAnchor.constraint(equalTo: currentCountPerson.centerYAnchor),
+            stepperPerson.leftAnchor.constraint(equalTo: bottView.centerXAnchor),
+            stepperPerson.heightAnchor.constraint(equalTo: selectCountPercent.heightAnchor),
+            stepperPerson.widthAnchor.constraint(equalTo: bottView.widthAnchor, multiplier: 0.3)
+        ])
+        
+        stepperPerson.stepValue = 1
+        stepperPerson.value = self.startValuePerson
+        stepperPerson.addTarget(self, action: #selector(stepperTargetActions), for: .valueChanged)
+    }
+    
+    private func resultButtonSettings(){
+        resultButton = UIButton()
+        bottView.addSubview(resultButton)
+        resultButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            resultButton.bottomAnchor.constraint(equalTo: bottView.bottomAnchor, constant: -30),
+            resultButton.centerXAnchor.constraint(equalTo: bottView.centerXAnchor),
+            resultButton.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.06),
+            resultButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.4)
+        ])
+        
+        resultButton.setTitle("Рассчитать", for: .normal)
+        resultButton.setTitleColor(.white, for: .normal)
+        resultButton.backgroundColor = .systemGreen
+        resultButton.layer.cornerRadius = 20
+        
+        resultButton.addTarget(self, action: #selector(resultButtonAction), for: .touchUpInside)
+    }
+    
+    @objc func stepperTargetActions(){
+        currentCountPerson.text = "\(Int(stepperPerson.value))"
+    }
+    
+    @objc func resultButtonAction(){
+        var result = 0
+        guard let summ = Int(billTextField.text ?? "0") else { return }
+        result = summ / (Int(currentCountPerson.text ?? "1") ?? 1)
+        let alert = UIAlertController(title: "Счет на человека", message: "\(result)", preferredStyle: .alert)
+        let act = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(act)
+        present(alert, animated: true)
+    }
+    
+    
+    
 }
-
